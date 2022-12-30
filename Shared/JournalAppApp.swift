4a67f5ea82ff7cd63e4f6d5b583b0cf30fbcec9a
Calvin Robert Lee <calvinrobertlee@gmail.com>
2022-12-29 16:49:53 -0800

    Initial Commit

---
+++ Shared/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  Shared
+//
+//  Created by user on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
